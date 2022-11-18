d70f3a4089d4d6895b3f707277732acec1dce0b2
turnerr8 <turnerr8@wwu.edu>
2022-11-18 11:39:07 -0800

    adding files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "head.php";
+?>
+
+<!--NAVIGATION-->
+<nav>
+    <!--logo-->
+    <ul>
+        <li>Home</li>
+        <li>Wishlist</li>
+        <li>Secret Santa</li>
+    </ul>
+    <button>Log In</button>
+    <button>Sign Up</button>
+</nav>
+
+<section id="Wishlist">
+
+</section>
