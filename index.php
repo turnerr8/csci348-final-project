78de029a64d123ac86544b1105472f76ddc25f3a
turnerr8 <turnerr8@wwu.edu>
2022-11-19 17:56:36 -0800

    can view the database, working on inserting using ajax

--- index.php
+++ index.php
@@ -14,10 +14,40 @@ require_once "commonvars.php";
     
     </nav>
 
+    <!--ADD AN ITEM TO THE WISHLIST FORM-->
+
     <section id="Wishlist">
+        <form id="wishlist-adder">
+            <label for="itemName">Item:</label>
+            <input type="text" name="itemName" id="itemName">
+
+            <label for="itemLink">Item Link</label>
+            <input type="text" name="itemLink" id="itemLink">
+
+            <input type="submit" value="add" name="submit" id="submit">
+        </form>
+
+        <script>
+            //READ
+            //https://code.tutsplus.com/tutorials/submit-a-form-without-page-refresh-using-jquery--net-59
+            $('#Wishlist').submit(function(){
+                console.log("submit");
+                var postData = $('#Wishlist').serialize();
+                $.post('wishlist-add.php', postData, function(data) {
+                    console.log("added");
+                });
+            });
+        </script>
+
+
+    <!-- CONNECT TO DATABASE-->
     <?php
         $db = new PDO($databaseConnection, $databaseUname, $databasePassword);
+
+
+        echo "<h1>Wish List </h1>";
         $rows = $db->query("SELECT * FROM WishList JOIN Users ON (WishList.userId = Users.userId);");?>
+        <!--ADD ROWS OF WISHLIST-->
         <table>
             <tr>
                 <td>First</td>
