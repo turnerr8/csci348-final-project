47c8a3008734146919c83d917817477800eefde0
turnerr8 <turnerr8@wwu.edu>
2022-11-19 16:21:27 -0800

    wishlist pulls from the db

--- commonvars.php
+++ commonvars.php
@@ -1,5 +1,7 @@
 <?php
 
-$databaseConnection = "mysql:dbname=turnerr8_final_project;host=mysqlweb.cs.wwu.edu", "turnerr8", "-1COT_12cdl";
+$databaseConnection = "mysql:dbname=turnerr8_final_project;host=mysqlweb.cs.wwu.edu";
+$databaseUname = "turnerr8";
+$databasePassword= "-1COT_12cdl";
 
 ?>
--- index.php
+++ index.php
@@ -2,21 +2,42 @@
 require_once "head.php";
 require_once "commonvars.php";
 ?>
+<body>
+    <!--NAVIGATION-->
+    <nav>
+        <!--logo-->
+        <a href="#">Home</a>
+        <a href="#">Wishlist</a>
+        <a href="#">Secret Santa</a>
+        <a href="#">Log In</a>
+        <a href="#">Sign Up</a>
+    
+    </nav>
 
-<!--NAVIGATION-->
-<nav>
-    <!--logo-->
-    <ul>
-        <li>Home</li>
-        <li>Wishlist</li>
-        <li>Secret Santa</li>
-    </ul>
-    <button>Log In</button>
-    <button>Sign Up</button>
-</nav>
-
-<section id="Wishlist">
-<?php
-    $db = new PDO($databaseConnection);
-    ?>
-</section>
+    <section id="Wishlist">
+    <?php
+        $db = new PDO($databaseConnection, $databaseUname, $databasePassword);
+        $rows = $db->query("SELECT * FROM WishList JOIN Users ON (WishList.userId = Users.userId);");?>
+        <table>
+            <tr>
+                <td>First</td>
+                <td>Last</td>
+                <td>Item Name</td>
+                <td>Item link</td>
+            </tr>
+        <?php
+        foreach($rows as $row){
+            ?>
+            <tr>
+                <td><?=$row['firstName']?></td>
+                <td><?=$row['lastName']?></td>
+                <td><?=$row['itemName']?></td>
+                <td><a href=<?=$row['itemLink']?>><?=$row['itemName']?></a></td>
+            </tr>
+            <?php
+        }
+        
+        ?>
+        </table>
+    </section>
+</body>
