23f367b6c316a0a77e4bcdb1c27c3e9b7843c736
Katelyn Endicott <endicok@cf167-13.cs.wwu.edu>
2022-11-21 10:52:42 -0800

    CSS added

--- head.php
+++ head.php
@@ -11,4 +11,6 @@ session_start ();
     <title>Christmas Wishlist and Secret Santa Generator</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
+
+
     
--- signin.php
+++ signin.php
@@ -1,21 +1,77 @@
 <?php 
 include 'head.php';
-
-if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
-{
-    echo 'You are already signed in.<br>';
-    echo '<br><a href="page.php">Home</a><br>';
-    echo '<a href="signout.php">Sign out</a><br>';
-}
-else
-{
-    echo '<h3>Sign in</h3>';
-    echo '<form method="post" action="signedin.php">
-        <input type="hidden" name="activity" value="signin">
-        <label>Username: <input type="text" name="user_name"></label>
-        <label>Password: <input type="password" name="user_pass"></label>
-        <input type="submit" value="Sign in" />
-        </form>';
-}
-include 'footer.php';
 ?>
+
+<body>
+    <!--  nav-->
+
+    
+    <div class="wrapper1">
+
+        <header class ="logo"> Christmas Website </header>
+
+        <nav clas ="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">Wishlist</a></li>
+                <li><a href="">Secret Santa</a></li>
+            </ul>
+        </nav>
+
+        <div class="side-nav">
+            <ul>
+                <li class="item1"><a href="signin.php">Sign In</a></li>
+                <li class="item2"><a href="">Sign Up</a></li>
+            </ul>
+
+        </div>
+
+
+        <div class="main-content">
+            
+            <?php
+                if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
+                {
+                    echo 'You are already signed in.<br>';
+                    echo '<br><a href="page.php">Home</a><br>';
+                    echo '<a href="signout.php">Sign out</a><br>';
+                }
+                else
+                {
+                    echo '<h3>Sign in</h3>';
+                    echo '<form method="post" action="signedin.php">
+                        <input type="hidden" name="activity" value="signin">
+                        <label>Username: <input type="text" name="user_name"></label>
+                        <br>
+                        <label>Password: <input type="password" name="user_pass"></label>
+                        <br>
+                        <input type="submit" value="Sign in" />
+                        </form>';
+                }
+
+
+            ?>
+
+        </div>
+
+        <div class = "footer">
+            <?php
+            include 'footer.php';
+            ?>
+        </div>
+
+
+
+
+    </div>
+    
+
+    
+</body>
+
+
+
+
+
+
+
