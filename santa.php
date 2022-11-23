6e5f8cfa3fc5bae58a8c3a26055c048a93608bf1
nikita-rana <59716015+nikita-rana@users.noreply.github.com>
2022-11-23 13:52:54 -0800

    added basic framework still need main content

---
+++ santa.php
@@ -0,0 +1,49 @@
+<?php
+include head.php
+?>
+
+<body>
+    <div class="wrapper1">
+
+    <header class ="logo"> Christmas Website </header>
+
+    <nav clas ="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Wishlist</a></li>
+            <li><a href="">Secret Santa</a></li>
+        </ul>
+    </nav>
+
+    <div class="side-nav">
+        <ul>
+            <li class="item1"><a href="signin.php">Sign In</a></li>
+            <li class="item2"><a href="register.php">Sign Up</a></li>
+        </ul>
+
+    </div>
+
+
+    <div class="main-content">
+        <h1>Welcome to secret santa</h1>
+        <p>To register a group for secret santa please press the register button below</p>
+        
+
+
+
+
+
+    </div>
+
+    <div class = "footer">
+        <?php
+        include 'footer.php';
+        ?>
+    </div>
+
+
+
+
+    </div>
+
+</body>
