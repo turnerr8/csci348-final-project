c11faab3f8375492445ff8dcf4275f0f004a3569
nikita-rana <59716015+nikita-rana@users.noreply.github.com>
2022-11-29 18:22:08 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,28 @@ require_once "commonvars.php";
 ?>
 
 <body>
+
     <style>
         body{
-            background-image: url();
+            background-image: url(santa3.jpeg);
+            background-repeat: no-repeat;
+            background-size: cover;
         }
-     </style>
+    </style>
+
+    <div class="Wrap>">
+
+        <div class="haha">
+            <h1 style="color:green; font-family:papyrus; text-align: center; font-size: 40px "> WELCOME TO THE CHRISTMAS WEBSITE </h1>
+            <p style="color:green; font-family:papyrus; text-align: center; font-size: 30px">Use the links above to navigate to whishlists or secret santa </p>
+            
+
+        </div>
+
+
+
+    </div>
 
 </body>
+
 
