9b98c9d6e5b7fdd0b435fd7a268f4307225dda0e
Katelyn Endicott <endicok@cf165-19.cs.wwu.edu>
2022-12-01 20:13:20 -0800

    added css to signout page

--- signout.php
+++ signout.php
@@ -10,13 +10,38 @@ include 'commonvars.php';
 include 'nav.php';
 ?>
 
-<?php
-$_SESSION['signed_in'] = false;
-$_SESSION['user_id']    = "";
-$_SESSION['user_name']  = "";
-$_SESSION['user_level'] = 0;
-session_destroy();
+
+<body>
+    <style>
+        .main-content{
+            padding:0;
+            height: 43.2em;
+        }
+    </style>
+
+    <div class="main-content">
+
+
+        <div class="banner">
                 
-echo 'You have signed out. <br>';
+        </div>
+
+        <?php
+            $_SESSION['signed_in'] = false;
+            $_SESSION['user_id']    = "";
+            $_SESSION['user_name']  = "";
+            $_SESSION['user_level'] = 0;
+            session_destroy();
+                            
+            echo '<p class="message">You have signed out. </p><br>';
+            
+        ?>
+
+    </div>
+</body>
+
+<?php
 include 'footer.php';
 ?>
+
+
