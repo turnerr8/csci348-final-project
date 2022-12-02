e6937e9b6fe615797c49e8afae8e91c51a5fbc29
turnerr8 <turnerr8@wwu.edu>
2022-12-01 20:35:20 -0800

    fixed sign out

--- signout.php
+++ signout.php
@@ -10,30 +10,7 @@ include 'commonvars.php';
 include 'nav.php';
 ?>
 
-<<<<<<< HEAD
-<?php
-$_SESSION['signed_in'] = false;
-$_SESSION['fName']    = "";
-$_SESSION['lName']  = "";
-$_SESSION['userId'] = 0;
-session_destroy();
-=======
 
-<body>
-    <style>
-        .main-content{
-            padding:0;
-            height: 43.2em;
-        }
-    </style>
-
-    <div class="main-content">
-
-
-        <div class="banner">
->>>>>>> 18db50b23627ebda5dbae749908e7f3d10ea4f00
-                
-        </div>
 
         <?php
             $_SESSION['signed_in'] = false;
